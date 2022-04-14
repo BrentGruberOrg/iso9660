871860c5e7ad33486ea24255d765d41d76fee46b
BrentGruber <brent.gruber77@gmail.com>
2022-04-14 00:41:16 -0400

    beginning port of the library

--- src/iso9660.nim
+++ src/iso9660.nim
@@ -1,5 +1,6 @@
 # iso9660 implements reading and creating basic ISO9660 images
 
+import std/times
 
 # ISO 9660 Overview
 # https://archive.fo/xs9ac
@@ -22,12 +23,103 @@ const
   # "subject to agreement between the originator and the recipient of the volume"
   d1Characters*: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_!\"%&'()*+,-./:;<=>?"
 
+  dirFlagHidden*: int = 1
+  dirFlagDir*: int = 2
+  dirFlagAssociated*: int = 3
+  dirFlagRecord*: int = 4
+  dirFlagProtection*: int = 5
+  dirFlagMultiExtent*: int = 8
 
-# VolumeDescriptorHandler represents the data in bytes 0-6
-# of a Volume Descriptor as defined in ECMA-119 8.1
-type 
+
+type
+  # VolumeDescriptorHandler represents the data in bytes 0-6
+  # of a Volume Descriptor as defined in ECMA-119 8.1
   VolumeDescriptorHeader* = object
     volume_type*: byte
     identifier*: array[5, byte]
-    version*: byte
+    version*: byte  
 
+  # RecordingTimeStamp represents a time and date format
+  # that can be encoded according to ECMA-119 9.1.5
+  RecordingTimeStamp* = Time
+
+  # VolumeDescriptorTimestamp represents a time and date format
+  # that can be encoded according to ECMA-119 8.4.26.1
+  VolumeDescriptorTimestamp* = object 
+    year*: int
+    month*: int
+    day*: int
+    hour*:  int
+    minute*:  int
+    second*:  int
+    hundredth*: int
+    offset*:  int
+
+  # DirectoryEntry contains data from a Directory Descriptor
+  # as described by ECMA-119 9.1
+  DirectoryEntry* = object
+    extendedAttributeRecordLength*: byte
+    extentLocation*: int32
+    extentLength*: int32
+    recordingDateTime*: RecordingTimeStamp
+    fileFlags*: byte
+    fileUnitSize*:  byte
+    interleaveGap*: byte
+    volumeSequenceNumber*: int16
+    identifier*:  string
+    systemUse*: seq[byte]
+
+  # BootVolumeDescriptorBody represents the data in bytes 7-2047
+  # Of a Boot Record as defined in ECMA-119 8.2
+  BootVolumeDescriptorBody* = object
+    bootSystemIdentifier*: string
+    bootIdentifier*: string
+    bootSystemUse*: array[1977, byte]
+
+
+  # PrimaryVolumeDescriptorBody represents the data in bytes 7-2047
+  # of a Primary Volume Descriptor as defined in ECMA-119 8.4
+  PrimaryVolumeDescriptorBody* = object
+    systemIdentifier*: string 
+    volumeIdentifier*: string
+    volumeSpaceSize*: int32  
+    volumeSetSize*: int16 
+    volumeSequenceNumber*: int16
+    logicalBlockSize*: int16
+    pathTableSize*: int32
+    typeLPathTableLoc*: int32
+    optTypeLPathTableLoc*: int32
+    typeMPathTableLoc*: int32
+    optTypeMPathTableLoc*: int32
+    rootDirectoryEntry*: DirectoryEntry
+    volumeSetIdentifier*: string
+    publisherIdentifier*: string
+    dataPreparerIdentifier*: string
+    applicationIdentifier*: string
+    copyrightFileIdentifier*: string
+    abstractFileIdentifier*: string
+    bibliographicFileIdentifier*: string
+    volumeCreationDateAndTime*: VolumeDescriptorTimestamp
+    volumeModificationDateAndTIme*: VolumeDescriptorTimestamp
+    volumeExpirationDateAndTime*: VolumeDescriptorTimestamp
+    volumeEffectiveDateAndTime*:  VolumeDescriptorTimestamp
+    fileStructureVersion*:  byte
+    applicationUsed*: array[512, byte]
+
+
+proc UnMarshalBinary(vdh: var VolumeDescriptorHeader, data: seq[byte]): void =
+  if len(data) < 7:
+    raise EOFError
+
+  vdh.volume_type = data[0]
+  vdh.identifier = data[1:6]
+  vdh.version = data[6]
+
+proc MarshalBinary(vdh: var VolumeDescriptorHeader): seq[byte] =
+  var data:seq[byte] = newSeq[byte](7)
+  data[0] = vdh.volume_type
+  data[6] = vdh.version
+  for i in 1 .. 6:
+    data[i] = vdh.identifier[i-1]
+  
+  return data
